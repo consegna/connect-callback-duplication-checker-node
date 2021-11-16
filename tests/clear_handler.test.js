0ac0314ffc81c7e7f6f60508e191230f3d5f489a
Justin Adam Susans <justin.susans@consegna.cloud>
2021-11-16 16:09:15 +1300

    tests: Unit Tests of AWS via Mocks
    Add Mocks to AWS Services via the appropriate V3 compatible library
    Handle all regular states (there are more to be added such as
    service errors).
    SLight logic rearragements to better suit changes

--- tests/clear_handler.test.js
+++ tests/clear_handler.test.js
@@ -1,12 +1,21 @@
 const fs = require('fs')
+const { mockClient } = require('aws-sdk-client-mock');
+const {
+    DynamoDBDocumentClient, GetCommand,
+    DeleteCommand,
+} = require('@aws-sdk/lib-dynamodb');
+process.env.CALLBACKTABLE = "fakeTable";
+const handler = require('../index')
 
-// We grab the handler on each invocation to ensure we set 
-// the ENV vars correctly, otherwise they always use the default
+const ddbMock = mockClient(DynamoDBDocumentClient);
+
+beforeEach(() => {
+    ddbMock.reset();
+});
 
 /*
-TODO: Use Mocking Library to create appropriate mocking, such as
-https://www.npmjs.com/package/aws-sdk-mock and
-https://aws.amazon.com/blogs/developer/mocking-modular-aws-sdk-for-javascript-v3-in-unit-tests/
+About Mocking: See 
+https://m-radzikowski.github.io/aws-sdk-client-mock/#dynamodb-documentclient
 */
 describe('Failure Tests for clear_handler', () => {
     const OLD_ENV = process.env;
@@ -21,6 +30,7 @@ describe('Failure Tests for clear_handler', () => {
 
     test('Run Clear Handler No Empty Env Var', async () => {
         process.env.CALLBACKTABLE = null
+        // Explicitly grab the handler here so the ENV var is UNSET
         const functionToTest = (require('../index')).clear_handler;
         await expect(functionToTest({}, null))
             .rejects
@@ -28,13 +38,55 @@ describe('Failure Tests for clear_handler', () => {
     });
 
     test('Run Clear Handler No Parameters', async () => {
-        process.env.CALLBACKTABLE = "fakeTable";
         let eventFile = fs.readFileSync('./events/bad_no_params.json');
         let event = JSON.parse(eventFile);
-        const functionToTest = (require('../index')).clear_handler;
-        let response = await functionToTest(event, null);
+        let response = await handler.clear_handler(event, null);
         expect(response.result).toEqual('FAIL');
         expect(response.message).toEqual('ERROR_ENCOUNTERED');
         expect(response.duplicate).toBeFalsy();
+    });
+});
+
+describe('Success Tests for clear_handler', () => {
+    test('Run Clear Handler No Entry', async () => {
+        ddbMock.on(GetCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+        }).resolves({
+            Item: undefined
+        })
+        let eventFile = fs.readFileSync('./events/good_all_content.json');
+        let event = JSON.parse(eventFile);
+        let response = await handler.clear_handler(event, null);
+        expect(response.result).toEqual('SUCCESS');
+        expect(response.message).toEqual('NO_ACTION');
+    });
+
+    test('Run Clear Handler Existing Entry', async () => {
+        ddbMock.on(GetCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+        }).resolves({
+            Item: {
+                callback_number: "+642101234567",
+                original_request_id: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+                created_at: 1637029856,
+                ttl: 1637634656
+            }
+        })
+        ddbMock.on(DeleteCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+        }).resolves({
+            callback_number: "+642101234567",
+            original_request_id: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+            created_at: 1637029856,
+            ttl: 1637634656
+        })
+        let eventFile = fs.readFileSync('./events/good_all_content.json');
+        let event = JSON.parse(eventFile);
+        let response = await handler.clear_handler(event, null);
+        expect(response.result).toEqual('SUCCESS');
+        expect(response.message).toEqual('NUMBER_CLEARED');
     });
 });
--- tests/set_handler.test.js
+++ tests/set_handler.test.js
@@ -1,12 +1,20 @@
 const fs = require('fs')
+const { mockClient } = require('aws-sdk-client-mock');
+const {
+    DynamoDBDocumentClient, GetCommand,
+    UpdateCommand,
+} = require('@aws-sdk/lib-dynamodb');
+process.env.CALLBACKTABLE = "fakeTable";
+const handler = require('../index')
 
-// We grab the handler on each invocation to ensure we set 
-// the ENV vars correctly, otherwise they always use the default
+const ddbMock = mockClient(DynamoDBDocumentClient);
 
+beforeEach(() => {
+    ddbMock.reset();
+});
 /*
-TODO: Use Mocking Library to create appropriate mocking , such as
-https://www.npmjs.com/package/aws-sdk-mock and
-https://aws.amazon.com/blogs/developer/mocking-modular-aws-sdk-for-javascript-v3-in-unit-tests/
+About Mocking: See 
+https://m-radzikowski.github.io/aws-sdk-client-mock/#dynamodb-documentclient
 */
 describe('Failure Tests for set_handler', () => {
     const OLD_ENV = process.env;
@@ -21,6 +29,7 @@ describe('Failure Tests for set_handler', () => {
 
     test('Run Set Handler No Empty Env Var', async () => {
         process.env.CALLBACKTABLE = null
+        // Explicitly grab the handler here so the ENV var is UNSET
         const functionToTest = (require('../index')).set_handler;
         await expect(functionToTest({}, null))
             .rejects
@@ -28,13 +37,59 @@ describe('Failure Tests for set_handler', () => {
     });
 
     test('Run Set Handler No Parameters', async () => {
-        process.env.CALLBACKTABLE = "fakeTable";
         let eventFile = fs.readFileSync('./events/bad_no_params.json');
         let event = JSON.parse(eventFile);
-        const functionToTest = (require('../index')).set_handler;
-        let response = await functionToTest(event, null);
+        let response = await handler.set_handler(event, null);
         expect(response.result).toEqual('FAIL');
         expect(response.message).toEqual('ERROR_ENCOUNTERED');
+        expect(response.duplicate).toBeFalsy();
+    });
+});
+
+describe('Success Tests for set_handler', () => {
+    test('Run Set Handler Existing Entry', async () => {
+        ddbMock.on(GetCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+        }).resolves({
+            Item: {
+                callback_number: "+642101234567",
+                original_request_id: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+                created_at: 1637029856,
+                ttl: 1637634656
+            }
+        })
+        let eventFile = fs.readFileSync('./events/good_all_content.json');
+        let event = JSON.parse(eventFile);
+        let response = await handler.set_handler(event, null);
+        expect(response.result).toEqual('FAIL');
+        expect(response.message).toEqual('CALLBACK_EXISTS');
+        expect(response.duplicate).toBeTruthy();
+    });
+
+    test('Run Set Handler No Entry', async () => {
+        ddbMock.on(GetCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+        }).resolves({
+            Item: undefined
+        })
+        ddbMock.on(UpdateCommand, {
+            TableName: 'fakeTable',
+            Key: { 'callback_number': '+642101234567' },
+            // There is more to this payload but this should illustrate
+            // the behaviour
+        }).resolves({
+            callback_number: "+642101234567",
+            original_request_id: "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+            created_at: 1637029856,
+            ttl: 1637634656
+        })
+        let eventFile = fs.readFileSync('./events/good_all_content.json');
+        let event = JSON.parse(eventFile);
+        let response = await handler.set_handler(event, null);
+        expect(response.result).toEqual('SUCCESS');
+        expect(response.message).toEqual('TABLE_UPDATED');
         expect(response.duplicate).toBeFalsy();
     });
 });
